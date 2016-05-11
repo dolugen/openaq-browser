89b07b358706762631c23ff5757342e0080d365c
Dolugen Buuralda <dulguun@shine.mn>
2016-05-11 14:09:49 +0900

    [grunt] Update gh-pages.

--- app/endpoints/measurements.js
+++ app/endpoints/measurements.js
@@ -5,7 +5,7 @@
         .module('app.endpoints')
         .controller('MeasurementsController', MeasurementsController);
 
-    function MeasurementsController($http, $scope, URLService, dataService) {
+    function MeasurementsController($http, $scope, $filter, URLService, dataService) {
         var uri = URI(URLService.getUrl('measurements'));
         var params = {};
         $scope.query_url = uri.toString();
@@ -14,13 +14,39 @@
         $scope.busy = 0;
 
         $scope.updateUrl = function(model) {
-            $scope.query_url = URLService.updateUrl(uri, model, $scope[model]);
+            var urlValue = $scope[model];
+            if(model === "include_fields"){
+                urlValue = formatIncludeFields();
+            }
+            if(["date_from", "date_to"].indexOf(model) > -1){
+                urlValue = formatDateField(model);
+            }
+            $scope.query_url = URLService.updateUrl(uri, model, urlValue);
             if($scope[model]) {
-                params[model] = $scope[model];
+                params[model] = urlValue;
             } else {
                 delete params[model];
             }
         };
+
+        function getIncludeFields() {
+            var model = $scope.include_fields;
+            var include_fields = [];
+            for(var i = 0; i < Object.keys(model).length; i++){
+                if(model[Object.keys(model)[i]] === true){
+                    include_fields.push(Object.keys(model)[i]);
+                }
+            }
+            return include_fields;
+        }
+
+        function formatIncludeFields() {
+            return getIncludeFields().join();
+        }
+
+        function formatDateField(model) {
+            return $filter('date')($scope[model], "yyyy-MM-dd");
+        }
 
         $scope.get_locations = function() {
             var params = {};
